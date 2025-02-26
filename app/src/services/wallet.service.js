02b1c21319090c005785784771df11e19e2a2b1b
fanstrong <kingindoslayer@gmail.com>
2025-02-27 02:48:19 +0700

    Update wallet.service.js

--- app/src/services/wallet.service.js
+++ app/src/services/wallet.service.js
@@ -1,7 +1,16 @@
+import { ethers } from "ethers";
 import { accounts } from "../../../accounts/accounts.js";
 
 function loadWallets() {
-  return accounts;
+  return accounts.map((privateKey) => {
+    try {
+      const wallet = new ethers.Wallet(privateKey);
+      return wallet.address; 
+    } catch (error) {
+      console.error("Invalid private key:", privateKey);
+      return null;
+    }
+  }).filter(Boolean);
 }
 
 export default { loadWallets };
