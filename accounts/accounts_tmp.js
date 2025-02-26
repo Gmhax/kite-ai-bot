91c8bdb6037648329052888896dca5c74bd745fc
Rambeboy <kingindoslayer@gmail.com>
2025-02-27 02:15:37 +0700

    first commit

---
+++ accounts/accounts_tmp.js
@@ -0,0 +1,11 @@
+/**
+ * Private key list file
+ * Write your private key here like this
+ * export const accounts = [
+ *   "PK1",
+ *   "PK2",
+ *   "PK3",
+ * ];
+ */
+
+export const accounts = [];
---
+++ config/config_tmp.js
@@ -0,0 +1,5 @@
+export const groqConfig = {
+  apiKey: "your-groq-api-key-here",
+  model: "mixtral-8x7b-32768",
+  temperature: 0.7,
+};
