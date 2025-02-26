89ecdefcfa92bae06c72bb6257a3067fd397238e
fanstrong <kingindoslayer@gmail.com>
2025-02-27 02:46:17 +0700

    Update wallet.service.js

--- app/src/services/wallet.service.js
+++ app/src/services/wallet.service.js
@@ -1,30 +1,7 @@
-import fs from "fs";
-import path from "path";
-import { Wallet } from "ethers";
+import { accounts } from "../../../accounts/accounts.js";
 
 function loadWallets() {
-  try {
-    const filePath = path.resolve("../../../accounts/accounts.js");
-
-    if (!fs.existsSync(filePath)) {
-      throw new Error(`File not found: ${filePath}`);
-    }
-
-    const data = fs.readFileSync(filePath, "utf-8");
-    const privateKeys = JSON.parse(data); 
-
-    return privateKeys.map((privateKey) => {
-      try {
-        return new Wallet(privateKey).address;
-      } catch (error) {
-        console.error("Invalid private key:", error.message);
-        return null;
-      }
-    }).filter(Boolean);
-  } catch (error) {
-    console.error("Error loading wallets:", error.message);
-    return [];
-  }
+  return accounts;
 }
 
 export default { loadWallets };
